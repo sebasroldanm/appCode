1018a0f8ac8cb3a87b6ae449ff9688a9bd0abd9c
Sebastian Roldan <sebasroldanm@gmail.com>
2020-07-28 14:21:21 -0500

    Conexión BD
    CREATE TABLE `users` (
      `id` int NOT NULL AUTO_INCREMENT,
      `name` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
      `username` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
      `password` text COLLATE utf8_spanish_ci NOT NULL,
      `role` int NOT NULL,
      `deleted` int DEFAULT '0',
      `last_login` date DEFAULT NULL,
      PRIMARY KEY (`id`)
    ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1,30 +1,20 @@
 <?php
 
 namespace App\Controllers;
+/* use CodeIgniter\Controller; */
+
+use App\Models\UsersModel;
 
 class Dashboard extends BaseController
 {
     public function index()
     {
-        $data['mydata1'] = "testing1";
-        $data['mydata2'] = "testing2";
-        return view('form', $data);
-    }
-
-    protected function helloWord($slug = null, $id = null)
-    {
-        echo $slug . "<br>";
-        echo $id . "<br>";
-    }
-
-    public function template(){
-        $parser = \Config\Services::parser();
-        $data=[
-            'title' => 'My Website Title',
-            'content' => 'laudantium aut quibusdam',
-            'footer' => 'Goodbye'
-        ];
-
-        echo $parser->setData($data)->render('template');
+        $model = new UsersModel();
+        $id = $model->insert([
+            "name" => "Sebastian",
+            "username" => "sebasroldanm",
+            "password" => "123456",
+            "role" => "1"
+        ]);
     }
 }
---
+++ app/Models/UsersModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UsersModel extends Model
+{
+    protected $table = "users";
+    protected $primaryKey = "id";
+
+    protected $returnType = "array";
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = ["name", "username", "password", "role", "last_login"];
+}
