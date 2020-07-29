9bfa929e9e4b67bc3a884425ba898c3ea8deceae
Sebastian Roldan <sebasroldanm@gmail.com>
2020-07-29 18:58:08 -0500

    Traer VIstas

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -9,16 +9,14 @@ class Dashboard extends BaseController
 {
     public function index()
     {
+        $this->loadViews("index");
+    }
+
+    public function loadViews($view = null)
+    {
         echo view("includes/header");
-        echo view("index");
+        echo view($view);
         echo view("includes/extra");
         echo view("includes/footer");
-        /* $model = new UsersModel();
-        $id = $model->insert([
-            "name" => "Sebastian",
-            "username" => "sebasroldanm",
-            "password" => "123456",
-            "role" => "1"
-        ]); */
     }
 }
