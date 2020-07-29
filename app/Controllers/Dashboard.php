0e310caaf98238c440f2283ac2c4f031cd5069da
Sebastian Roldan <sebasroldanm@gmail.com>
2020-07-29 18:54:08 -0500

    Plantilla Blog

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -9,12 +9,16 @@ class Dashboard extends BaseController
 {
     public function index()
     {
-        $model = new UsersModel();
+        echo view("includes/header");
+        echo view("index");
+        echo view("includes/extra");
+        echo view("includes/footer");
+        /* $model = new UsersModel();
         $id = $model->insert([
             "name" => "Sebastian",
             "username" => "sebasroldanm",
             "password" => "123456",
             "role" => "1"
-        ]);
+        ]); */
     }
 }
